933d41580fe732b239d6fb68b3364a77be6cfc30
Carsten Schlote <schlote@vahanus.net>
2012-09-14 18:58:27 +0200

    [build-script] Added support for ptxdist submodule
    - Added code to checkout, update and build ptxdist from submodule
      according to reported required version
    - Uses --info option of fixme-script to obtain ptxdist version
    
    Signed-off-by: Carsten Schlote <schlote@vahanus.net>

--- build_all_v2.mk
+++ build_all_v2.mk
@@ -34,6 +34,7 @@ $(error failed to detect arch, or arch is unsupported)
 endif
 
 PTXDIST			:= ./p --force
+PTXDIST_VERSION_REQUIRED := $(shell ./fixup_ptxconfigs.sh --info)
 
 ifdef BENICE
 NICE			+= nice -n 19
@@ -80,10 +81,10 @@ OLDCONFIGS	:= $(foreach config,$(CONFIGS_),$(addsuffix .oldconfig,$(config)))
 
 all: $(TBZ2S) $(DEBS)
 
-$(DEB_PREFIX)%$(DEB_SUFFIX): $(STATEDIR)/%.build | mkdirs
+$(DEB_PREFIX)%$(DEB_SUFFIX): $(STATEDIR)/%.build | compile-ptxd mkdirs
 	@scripts/make_deb.sh -d "$(@)" -s "$(PTX_AUTOBUILD_DESTDIR)/$(2INSTDIR_$(*))"
 
-$(TBZ2_PREFIX)%$(TBZ2_SUFFIX): $(STATEDIR)/%.build | mkdirs
+$(TBZ2_PREFIX)%$(TBZ2_SUFFIX): $(STATEDIR)/%.build | compile-ptxd mkdirs
 	@echo Creating $(notdir $@) ...
 	@echo 'tar -C "$(PTX_AUTOBUILD_DESTDIR)/opt" --exclude=gcc-first -cjf "$(@)" "$(patsubst /opt/%,%,$(2INSTDIR_$(*)))"' | fakeroot
 
@@ -109,11 +110,39 @@ oldconfig: $(OLDCONFIGS)
 %.oldconfig:
 	$(PTXDIST) oldconfig --ptxconfig=$(2CONFIGFILE_$(*))
 
+$(STATEDIR)/ptxdist.build:
+	@git submodule update || (echo "Unable to update GIT submodules"; false )
+	@./p --version 2&> /dev/null || ( \
+		echo "building ptxdist binary in subdir 'ptxdist'."; \
+		cd ptxdist; ./autogen.sh; ./configure --prefix=`pwd`; \
+		make; \
+		cd ..; )
+	@./p --version 2&> /dev/null || (echo "Unable to build ptxdist."; false)
+	@./p --version 2&> $@
+	@if [ "`cat $@`" != $(PTXDIST_VERSION_REQUIRED) ]; then \
+		echo -en "\nRelease mismatch!\n\n"; \
+		echo "Required ptxdist version:" $(PTXDIST_VERSION_REQUIRED); \
+		echo "Compiled ptxdist version:" `cat $@`; \
+		echo -en "\n\nUpdate ptxdist submodule to required version\n"; \
+		echo -en "Use ./build_all_v2.sh update-ptxd\n"; \
+	fi
+	@echo -e "\n\nSuccessfully prepared ptxdist $(PTXDIST_VERSION_REQUIRED) for\nOSELAS.Toolchain-$(VERSION) build.\n\n"
+
 mkdirs:
 	@mkdir -p $(STATEDIR) $(DISTDIR)
 
 print-%:
 	@echo "$* is \"$($(*))\""
+
+
+compile-ptxd: mkdirs $(STATEDIR)/ptxdist.build
+
+update-ptxd: mkdirs
+	-make -C ptxdist distclean
+	rev=`./fixup_ptxconfigs.sh --info`; \
+	cd ptxdist && git checkout ptxdist-$$rev
+	git add ptxdist
+	git citool
 
 help:
 	@echo -e "\nAvailable DPKG targets:\n"
