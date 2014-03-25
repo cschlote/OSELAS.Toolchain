4969cd288d9e5b71a88ed5addfd7155847c0aa29
Buildmaster <buildmaster@konzeptpark.intra>
2014-03-25 14:57:20 +0100

    build-script: Added support for ptxdist submodule, added clean targets and extra help
    - Added code to checkout, update and build ptxdist from submodule
      according to reported required version
    - Uses --info option of fixme-script to obtain ptxdist version
    - Added update-configs, clean and distclean targets
    - Simplifies some standard tasks
    
    Signed-off-by: Carsten Schlote <schlote@vahanus.net>

--- build_all_v2.mk
+++ build_all_v2.mk
@@ -22,6 +22,8 @@ ARG			:= images
 export PTXDIST_ENV_WHITELIST	:= CROSS_GDB_WITHOUT_PYTHON
 export CROSS_GDB_WITHOUT_PYTHON	?= y
 
+PTXDIST_VERSION_REQUIRED := $(shell ./fixup_ptxconfigs.sh --info)
+
 ifdef BENICE
 NICE			+= nice -n 19
 endif
@@ -45,7 +47,7 @@ OLDCONFIGS	:= $(foreach config,$(CONFIGS_),$(addsuffix .oldconfig,$(config)))
 all: $(BUILDS)
 
 $(foreach config,$(CONFIGS_),$(eval $(STATEDIR)/$(config).build: $(2CONFIGFILE_$(config))))
-$(STATEDIR)/%.build: | mkdirs
+$(STATEDIR)/%.build: | mkdirs $(STATEDIR)/ptxdist.build
 	@echo "building ${*}"
 	$(NICE) $(PTXDIST) $(ARG) --ptxconfig=$(2CONFIGFILE_$(*))
 	@if [ "$(strip $(filter images,$(ARG)))" = "images" ]; then touch "$@"; fi
@@ -55,12 +57,65 @@ oldconfig: $(OLDCONFIGS)
 %.oldconfig:
 	$(PTXDIST) oldconfig --ptxconfig=$(2CONFIGFILE_$(*))
 
+.PHONY: $(STATEDIR)/ptxdist.build
+$(STATEDIR)/ptxdist.build:
+	@git submodule update || (echo "Unable to update GIT submodules"; false)
+	@./p --version 2&> /dev/null || ( \
+		echo "building ptxdist binary in subdir 'ptxdist'."; \
+		cd ptxdist; ./autogen.sh; ./configure --prefix=`pwd`; \
+		make; \
+		cd ..; )
+	@./p --version 2&> /dev/null || (echo "Unable to build ptxdist."; false)
+	@./p --version 2&> $@
+	@if [ "`cat $@`" != $(PTXDIST_VERSION_REQUIRED) ]; then \
+		echo -en "\nRelease mismatch!\n\n"; \
+		echo "Required ptxdist version:" $(PTXDIST_VERSION_REQUIRED); \
+		echo "Compiled ptxdist version:" `cat $@`; \
+		echo -en "\n\nUpdate ptxdist submodule to required version\n"; \
+		echo -en "Use ./build_all_v2.sh update-ptxd\n"; \
+	fi
+	@echo -e "\n\nSuccessfully prepared ptxdist $(PTXDIST_VERSION_REQUIRED) for\nOSELAS.Toolchain-$(VERSION) build.\n\n"
+
 mkdirs:
 	@mkdir -p $(STATEDIR) $(DISTDIR)
 
 print-%:
 	@echo "$* is \"$($(*))\""
 
+clean-ptxd :
+	-make -C ptxdist distclean
+	-if [ -e $(STATEDIR)/ptxdist.build ] ; then rm $(STATEDIR)/ptxdist.build; fi
+
+compile-ptxd: mkdirs $(STATEDIR)/ptxdist.build
+
+update-ptxd: mkdirs clean-ptxd
+	@echo "Checkout ptxdist version \'ptxdist-$$rev\'."
+	rev=`./fixup_ptxconfigs.sh --info`; if [ -n "$$rev" ] ; then \
+		    cd ptxdist && git checkout ptxdist-$$rev || exit 10; \
+	fi
+	@echo "Check for changed ptxdist submodule. Commit if changed."
+	git status ptxdist || (git add ptxdist ; git citool)
+	@echo "Updated ptxdist to version $(OSELAS_VERSION), prepared recompile"
+
+update-configs: compile-ptxd
+	./fixup_ptxconfigs.sh
+
+clean:
+	-make -C ptxdist distclean
+	-rm -rf platform-*
+	-rm -rf $(STATEDIR) $(PTX_AUTOBUILD_DESTDIR)
+
+distclean: clean clean-ptxd
+	-rm -rf $(DISTDIR)
+
 help:
+	@echo "OSELAS_VERSION: $(OSELAS_VERSION)"
+	@echo "PTXDIST_VERSION_REQUIRED: $(PTXDIST_VERSION_REQUIRED)"
+	@echo ""
+	@echo "Available make targets:"
+	@echo "  all clean compile-ptxd distclean help mkdirs oldconfig"
+	@echo "  update-configs update-ptxd clean-ptxd"
+	@echo ""
 	@echo "Available build targets:"
 	@for i in $(sort $(BUILDS)); do echo $$i; done;
+
