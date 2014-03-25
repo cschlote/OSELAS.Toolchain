367e0351253ebdd3c468d73358c6399ce5f48e9d
Buildmaster <buildmaster@konzeptpark.intra>
2014-03-25 14:54:46 +0100

    fixup-script: Added m68, geode and atom to list of know variants
    - Allows for optimized target compilers on x86
    - Adds support for m68k
    
    Signed-off-by: Carsten Schlote <schlote@vahanus.net>

--- fixup_ptxconfigs.sh
+++ fixup_ptxconfigs.sh
@@ -114,6 +114,9 @@ fixup()
 	i?86*)
 	    PTXCONF_ARCH_I386=y
 	    ;;
+	m68k*)
+	    PTXCONF_ARCH_M68K=y
+	    ;;
 	mips*)
 	    PTXCONF_ARCH_MIPS=y
 	    ;;
@@ -204,12 +207,18 @@ fixup()
 	    ;;
 
 	x86_64-unknown-linux-gnu)
+		;;
+
+	m68k-linux-gnu)
+	    PTXCONF_CROSS_GCC_CONFIG_EXTRA="--with-cpu=m68030"
+	    PTXCONF_GLIBC_CONFIG_EXTRA="--without-fp"
 	    ;;
 
 	mipsel-softfloat-linux-gnu)
 	    PTXCONF_CROSS_GCC_CONFIG_EXTRA="--with-float=soft"
 	    PTXCONF_GLIBC_CONFIG_EXTRA="--without-fp"
 	    ;;
+
 	powerpc-603e-linux-gnu)
 	    PTXCONF_CROSS_GCC_CONFIG_EXTRA="--with-cpu=603e	--disable-altivec"
 	    ;;
