2df1f9d343569a5289e25f0cdf5e84aa5e671878
Buildmaster <buildmaster@konzeptpark.intra>
2014-03-25 14:56:53 +0100

    build-script: Modified nice behaviour
    - Just to be work in background instead of full foreground
    
    Signed-off-by: Carsten Schlote <schlote@vahanus.net>

--- build_all_v2.mk
+++ build_all_v2.mk
@@ -40,7 +40,7 @@ export PTXDIST_ENV_WHITELIST	:= CROSS_GDB_WITHOUT_PYTHON
 export CROSS_GDB_WITHOUT_PYTHON	:= y
 
 ifdef BENICE
-NICE			+= nice -20
+NICE			+= nice -n 19
 endif
 
 CONFIGDIR	:= ptxconfigs
