f8371bf7c9cb6b82961b3f486abf66cd94024d16
Carsten Schlote <schlote@vahanus.net>
2016-06-23 15:32:47 +0200

    fixup-script: Changed the PTXCONF_PROJECT for vahanus projects
    - Added suffix to mark version for vahanus projects
    
    Signed-off-by: Carsten Schlote <schlote@vahanus.net>

--- fixup_ptxconfigs.sh
+++ fixup_ptxconfigs.sh
@@ -36,7 +36,7 @@ fixup()
     local config="${1}"
 
     # version
-    PTXCONF_PROJECT="OSELAS.Toolchain-2016.06.1"
+    PTXCONF_PROJECT="OSELAS.Toolchain-2016.06.1.0"
     PTXCONF_PROJECT="${PTXCONF_PROJECT##*/}"
 
     # defaults
